c75f3f6c23c5e894e6c680ae2261f3fb34a63156
coffee-Maker19 <125376601+coffee-Maker19@users.noreply.github.com>
2025-07-01 12:44:53 +0800

    Complete Admin Setup and Rendered Data from Database

--- CRUD/MyCrud/urls.py
+++ CRUD/MyCrud/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path #impoert path from django.urls
 from . import views #import views.py from the current directory
 
+
 urlpatterns = [
     path("", views.index, name="index"), # map the index view to the root URL
+    path("todo/", views.show_todo_items, name="show_todo_items"), # map the show_todo_items view to the /todo/ URL
 ]
--- CRUD/MyCrud/views.py
+++ CRUD/MyCrud/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, HttpResponse
+from . models import TodoItem #import the TodoItem model from models.py in the current directory
 
 # Create your views here.
 def index(request):
     return render(request, "home.html")
+
+def show_todo_items(request):
+    list = TodoItem.objects.all()  # Get all TodoItem objects from the database
+    return render(request, "todo_list.html", {"todos" : list}) # Render the todo_list.html template with the list of TodoItem objects
